2b10443b529c5cbd19363fd1a1b1858ea5b09399
Shintani Teppei <teppest1215@gmail.com>
2017-12-15 02:02:24 +0900

    rename

--- wordcloud.py
+++ wordcloud.py
@@ -6,16 +6,15 @@ import os
 
 def create_wordcloud(text):
     # 環境に合わせてフォントのパスを指定する。
-    #fpath = "/System/Library/Fonts/HelveticaNeue-UltraLight.otf"
     fpath = "/Library/Fonts/ヒラギノ丸ゴ ProN W4.ttc"
 
     # ストップワードの設定
     stop_words = [ 'てる', 'いる', 'なる', 'れる', 'する', 'ある', 'こと', 'これ', 'さん', 'して',
              'くれる', 'やる', 'くださる', 'そう', 'せる', 'した',  '思う',
              'それ', 'ここ', 'ちゃん', 'くん', '', 'て', 'に', 'を', 'は', 'の', 'が', 'と', 'た', 'し', 'で',
-             'ない', 'も', 'な', 'い', 'か', 'ので', 'よう', 'ブック', '開く', '平成', '年度',
+             'ない', 'も', 'な', 'い', 'か', 'ので', 'よう', 'たち', 'いただき', 'ブック', '開く', '平成', '年度',
              '委員', '高専', 'KOUSEN', 'KUREKOUSEN', 'でき', 'なり', 'あり', '入学', '案内', '参加',
-             'TOP', '最新', 'ページ', 'co', 'ba']
+             'TOP', '最新', 'ページ', 'co', 'ba', '開催', '担当', '学生', '今回']
 
     wordcloud = WordCloud(background_color="white",font_path=fpath, width=900, height=500, stopwords=set(stop_words)).generate(text)
 
@@ -31,11 +30,8 @@ for file in files:
     if file == ".DS_Store":
         continue
 
-    if 'diary|2017' in file:    
-        with open("wordlist/" + file, "r") as f:
-            line = f.readline().replace("\n", "")
-            wordlists.append(line)
+    with open("wordlist/" + file, "r") as f:
+        line = f.readline().replace("\n", "")
+        wordlists.append(line)
 
-# wordlist = get_wordlist_from_QiitaURL("https://www.kure-nct.ac.jp/")
-# print(wordlist)
 create_wordcloud(" ".join(wordlists))
