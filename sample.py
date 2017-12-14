feb73e1a0f3e3fc26fd8ade8353bafcdebaf283e
Shintani Teppei <teppest1215@gmail.com>
2017-12-14 22:19:08 +0900

    クロールできるようにした

--- sample.py
+++ sample.py
@@ -3,6 +3,9 @@ from wordcloud import WordCloud
 from bs4 import BeautifulSoup
 import requests
 import MeCab as mc
+import re
+import time
+from urllib.parse import urljoin
 
 def mecab_analysis(text):
     t = mc.Tagger('-Ochasen')
@@ -45,6 +48,43 @@ def create_wordcloud(text):
     plt.axis("off")
     plt.show()
 
-url = "https://www.kure-nct.ac.jp"
-wordlist = get_wordlist_from_QiitaURL(url)
-create_wordcloud(" ".join(wordlist))
+def extract_link_from_url(url):
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    urls = set()
+    for a in soup.find_all('a'):
+        href = urljoin(url, a.get('href'))
+        if re.match(r"^(https://www\.kure-nct\.ac\.jp/).*(?!pdf)$", href):
+            urls.add(href)
+
+    return urls
+
+def set_visited_urls(parent_url):
+    url_list = set()
+    url_list.add(parent_url)
+
+    while(not len(url_list) == 0):
+        url = url_list.pop()
+        urls = extract_link_from_url(url)
+        time.sleep(1)
+
+        visited_urls.add(url)
+        print(url)
+
+        not_visited_urls = urls - visited_urls
+
+        url_list = url_list | not_visited_urls
+
+    return
+
+
+parent_url = "https://www.kure-nct.ac.jp"
+visited_urls = set()
+set_visited_urls(parent_url)
+
+print(visited_urls)
+
+# wordlist = get_wordlist_from_QiitaURL(url)
+# create_wordcloud(" ".join(wordlist))
