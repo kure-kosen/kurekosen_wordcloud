73a24c2fff82fe52005aa61f1ba7c69c91518b60
Shintani Teppei <teppest1215@gmail.com>
2017-12-15 00:26:15 +0900

    wordlistを書き出した

---
+++ get_wordlist.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from bs4 import BeautifulSoup
+import requests
+import MeCab as mc
+import re
+import time
+from urllib.parse import urljoin
+
+def mecab_analysis(text):
+    t = mc.Tagger('-Ochasen')
+    t.parse('')
+    node = t.parseToNode(text)
+    output = []
+    while(node):
+        if node.surface != "":  # ヘッダとフッタを除外
+            word_type = node.feature.split(",")[0]
+            if word_type in ["形容詞", "動詞","名詞", "副詞"]:
+                output.append(node.surface)
+        node = node.next
+        if node is None:
+            break
+    return output
+
+def get_wordlist_from_QiitaURL(url):
+    res = requests.get(url)
+    if not res.status_code == 200:
+        return []
+
+    res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    if soup.body == None:
+        return []
+    
+    text = soup.body.get_text().replace('\n','').replace('\t','')
+    return mecab_analysis(text)
+
+def main():
+    with open("url.txt", "r") as url_file:
+        url = url_file.readline()
+
+        while url:
+            url = url.replace("\n", '')
+            print(url)
+
+            wordlist = get_wordlist_from_QiitaURL(url)
+            if not len(wordlist) == 0:
+                with open("wordlist/" + url.replace("/", "|") + ".txt", "w") as word_file:
+                    word_file.write(" ".join(wordlist))
+                    time.sleep(1)
+
+            url = url_file.readline()
+
+if __name__ == '__main__':
+    main()
--- sample.py
+++ sample.py
@@ -7,30 +7,7 @@ import re
 import time
 from urllib.parse import urljoin
 
-def mecab_analysis(text):
-    t = mc.Tagger('-Ochasen')
-    t.parse('')
-    node = t.parseToNode(text)
-    output = []
-    while(node):
-        if node.surface != "":  # ヘッダとフッタを除外
-            word_type = node.feature.split(",")[0]
-            if word_type in ["形容詞", "動詞","名詞", "副詞"]:
-                output.append(node.surface)
-        node = node.next
-        if node is None:
-            break
-    return output
-
-def get_wordlist_from_QiitaURL(url):
-    res = requests.get(url)
-    res.encoding = res.apparent_encoding
-    soup = BeautifulSoup(res.text, "html.parser")
-    text = soup.body.get_text().replace('\n','').replace('\t','')
-    return mecab_analysis(text)
-
 def create_wordcloud(text):
-
     # 環境に合わせてフォントのパスを指定する。
     #fpath = "/System/Library/Fonts/HelveticaNeue-UltraLight.otf"
     fpath = "/Library/Fonts/ヒラギノ丸ゴ ProN W4.ttc"
@@ -39,7 +16,7 @@ def create_wordcloud(text):
     stop_words = [ 'てる', 'いる', 'なる', 'れる', 'する', 'ある', 'こと', 'これ', 'さん', 'して',
              'くれる', 'やる', 'くださる', 'そう', 'せる', 'した',  '思う',
              'それ', 'ここ', 'ちゃん', 'くん', '', 'て', 'に', 'を', 'は', 'の', 'が', 'と', 'た', 'し', 'で',
-             'ない', 'も', 'な', 'い', 'か', 'ので', 'よう', '']
+             'ない', 'も', 'な', 'い', 'か', 'ので', 'よう']
 
     wordcloud = WordCloud(background_color="white",font_path=fpath, width=900, height=500, stopwords=set(stop_words)).generate(text)
 
@@ -48,44 +25,6 @@ def create_wordcloud(text):
     plt.axis("off")
     plt.show()
 
-def extract_link_from_url(url):
-    res = requests.get(url)
-    res.encoding = res.apparent_encoding
-    soup = BeautifulSoup(res.text, "lxml")
-
-    urls = set()
-    for a in soup.find_all('a'):
-        href = urljoin(url, a.get('href'))
-        if re.match(r"^(https://www\.kure-nct\.ac\.jp/).*(\.html)$", href):
-            urls.add(href)
-
-    return urls
-
-def set_visited_urls():
-    url_list = set()
-    url_list.add("https://www.kure-nct.ac.jp/")
-    # IWだけaタグのリンク先でredirectさせててアクセスできてなかったので追加
-    url_list.add("https://www.kure-nct.ac.jp/incubation/H29_top/index.html")
-
-    while(not len(url_list) == 0):
-        url = url_list.pop()
-        urls = extract_link_from_url(url)
-        time.sleep(1)
-
-        visited_urls.add(url)
-        print(url)
-
-        not_visited_urls = urls - visited_urls
-
-        url_list = url_list | not_visited_urls
-
-    return
-
-visited_urls = set()
-set_visited_urls()
-
-with open("url.txt", "w") as file:
-    file.writelines("\n".join(visited_urls))
-
-# wordlist = get_wordlist_from_QiitaURL(url)
+# wordlist = get_wordlist_from_QiitaURL("https://www.kure-nct.ac.jp/")
+# print(wordlist)
 # create_wordcloud(" ".join(wordlist))
